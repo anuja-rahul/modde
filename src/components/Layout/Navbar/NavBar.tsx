accf17dc5af3ec5d13b00e6ed2aa1e0f8fca6c81
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-06-10 19:18:28 +0530

    fix

--- src/components/Layout/Navbar/NavBar.tsx
+++ src/components/Layout/Navbar/NavBar.tsx
@@ -4,7 +4,7 @@ import Modde from "./../../../app/assets/images/Modde.svg";
 
 export default function NavBar() {
   return (
-    <div className="Navbar sticky top-0 z-[999] w-[100vw] rounded-md bg-transparent backdrop-blur-sm py-0">
+    <div className="Navbar fixed top-0 z-[999] w-[100vw] rounded-xl bg-transparent backdrop-blur-[2px] py-0">
       <div className="flex h-full flex-row flex-nowrap items-center justify-between px-12 py-0">
         <div className="title logo w-1/2 text-2xl">
           <Image src={Modde} alt="Modde" width={100} />
